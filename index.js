1deab275d8503a456f56e79604d164eaa81c92f0
Eten <eten.uket@yale.edu>
2021-07-31 19:50:46 -0400

    commit tree 0de9aec05e7b7200f1cdcbf8fe4e96f3287ca6c5

---
+++ index.js
@@ -0,0 +1,15 @@
+//let divide = (x, y) => x / y;
+
+
+//console.log(divide(2000, 100));
+
+function divide() {
+    return 2000/100
+}
+
+let add = (x, y) => x + y;
+
+console.log(add(10, 20)); // 30;
+
+let square = num => num ** num;
+console.log(square(3, 6));
